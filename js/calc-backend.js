ae73fd5b323bf871519154892163ff8863b024ab
Valia Dempsey <valentinaml@gmail.com>
2017-11-17 23:21:18 -0800

    Write, fail, pass test for earthYears to seconds

--- js/calc-backend.js
+++ js/calc-backend.js
@@ -1,23 +1,38 @@
 
 export class Planet {
-  constructor(name) {
-    this.name = name;
+  constructor(name, years) {
+    this._name = name;
+    this._years = years;
   }
 
-  ageOnMars() {
-    let earthAgeInYears = 1;
-    let marsYears = parseInt(earthAgeInYears) * 1.88;
-    return marsYears;
-  }
+  // get years() {
+  //   return this._years;
+  // }
 
-   ageInSec() {
-    // const earthDayInSec = 86400;
-    // const earthYearInDays = 365;
-    const earthYearInSec = 20 //just for testing;
-    // 365 * 86400;
-    let earthAgeInYears = 1;
-    let earthAgeInSec = earthAgeInYears * earthYearInSec;
-    return earthAgeInSec;
-  }
+  // ageOnMars() {
+  //   let earthAgeInYears = 1;
+  //   let marsYears = parseInt(earthAgeInYears) * 1.88;
+  //   return marsYears;
+  // }
+  //
+  //  ageInSec(earthAgeInYears) {
+  //    return earthAgeInYears * 20;
+  //   // const earthDayInSec = 86400;
+  //   // const earthYearInDays = 365;
+  //   // const earthYearInSec = 20 //just for testing;
+  //   // // 365 * 86400;
+  //   // let earthAgeInYears = 1;
+  //   // let earthAgeInSec = earthAgeInYears * earthYearInSec;
+  //   // return earthAgeInSec;
+  //
+  // }
+
+ageInSec(earthAgeInYears) {
+    const earthDaysInYear = 365;
+    const earthDayInSec = 86400;
+    const earthYearInSec = 365 * 86400;
+    return earthAgeInYears * earthYearInSec;
+    // return output;
+ }
 
 };
--- spec/calc-spec.js
+++ spec/calc-spec.js
@@ -3,20 +3,21 @@ import { Planet } from './../js/calc-backend.js';
 describe("ageInSec", function() {
   // ageInSec
   it("should calculate entered age in seconds on Earth", function() {
-    let earthAgeInYears = 2;
+    let earthAgeInYears = 3;
     let earth = new Planet(earthAgeInYears);
-    expect(earth.ageInSec()).toEqual(40);
+    let output = earth.ageInSec(earthAgeInYears);
+    expect(output).toEqual(3 * 365 * 86400);
   });
 
 });
 
-describe("ageOnMars", function() {
+// describe("ageOnMars", function() {
+//
+//   it("should calculate entered age on Mars", function() {
+//     // let earthAgeInYears = " ";
+//     let earthAgeInYears = 1;
+//     let mars = new Planet(earthAgeInYears);
+//     expect(mars.ageOnMars()).toEqual(1.88);
+//   });
 
-  it("should calculate entered age on Mars", function() {
-    // let earthAgeInYears = " ";
-    let earthAgeInYears = 1;
-    let mars = new Planet(earthAgeInYears);
-    expect(mars.ageOnMars()).toEqual(1.88);
-  });
-
-});
+// });
