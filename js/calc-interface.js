55840d597cb3812a632d37a074c05394fda2143f
Valia Dempsey <valentinaml@gmail.com>
2017-11-22 13:54:27 -0800

    Edit birthday entry in index.html. Remove select menus. Create input for each instead

--- js/calc-interface.js
+++ js/calc-interface.js
@@ -11,8 +11,30 @@ $(document).ready(function() {
   });
 
 
+//month selection
+  $('#month').on('change', function(){
+    let selectedDate = this.value;
+    alert(selectedDate);
+  });
+
+//to create drop down selection for date
+  $("#date").click(function(){
+    let lastDate = 31,
+    select = document.getElementById('date');
+
+    for (let i = lastDate - 30; i<=lastDate; i++){
+      let opt = document.createElement('option');
+      opt.value = i;
+      opt.innerHTML = i;
+      select.appendChild(opt);
+      select.onchange = function(){
+        let selectedDate = select.options[select.selectedIndex].value;
+        alert(selectedDate);
+      };
+    }
+  });
 //to create drop down selection for year
-  // $("#year").click(function(event){
+  $("#year").click(function(){
   let currentYear = new Date().getFullYear(),
   select = document.getElementById('year');
 
@@ -22,10 +44,21 @@ $(document).ready(function() {
       opt.innerHTML = i;
       select.appendChild(opt);
       select.onchange = function(){
-        let selectedString = select.options[select.selectedIndex].value;
-        alert(selectedString);
+        let selectedYear = select.options[select.selectedIndex].value;
+        alert(selectedYear);
       };
-    // }
-  }
+    }
+  });
+
+  $("#ageBtn").click(function(event) {
+    // event.preventDefault();
+    let year = parseInt("1905");
+    let month = parseInt(selectedMonth);
+    let date = parseInt(selectedDate);
+    let newBirthday = new MyDate(selectedYear, selectedMonth, selectedDate);
+    let ageResult = newBirthday.ageYearToSec();
+    $("#ageOutput").text("You are " + ageResult + " seconds old");
+  });
+
 
 });
